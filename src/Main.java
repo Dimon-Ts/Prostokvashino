2b8bdbef4891868f4f0832b17a52a0f318293aa8
Dimon-Ts <tselishev.dimon@yandex.ru>
2022-09-12 13:59:23 +0300

    Трое из простоквашино.Шарик является Балбесом, по версии кота Матроскина.

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello Sharick!");
+        short bananas =  200;
+        System.out.println("бананов " + bananas + " кг ");
+        float sugar = 3;
+        float onePortion = sugar / 4;
+        System.out.println("одна порция сахара весит " + onePortion + " кг " );
+
+        byte a = 1;
+        short b = 1;
+        int c = 1;
+        long d = a + b + c;
+        System.out.println(d);
+
+        float g = a + 1f;
+        System.out.println(g);
+
+    }
+}
